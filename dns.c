b79ffd4df372ea61b479c5b1ff93c141f23f8380
Davidgp04 <104110970+Davidgp04@users.noreply.github.com>
2024-04-18 11:26:28 -0500

    Update dns.c

--- dns.c
+++ dns.c
@@ -71,7 +71,7 @@ int main(){
 
     //We place the local address
     struct addrinfo *bind_address;
-    getaddrinfo("18.212.62.23", "8080", &hints, &bind_address);
+    getaddrinfo("172.31.83.108", "8080", &hints, &bind_address);
 
 
      //We create the listener socket
