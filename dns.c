a3f60fa5fa93485949ef126a9c75d98658c9af31
Davidgp04 <104110970+Davidgp04@users.noreply.github.com>
2024-04-18 09:57:08 -0500

    Update dns.c

--- dns.c
+++ dns.c
@@ -12,9 +12,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct DNSRecord {
+  char domain[256];
+  char ip_address[16];
+};
+void agregarRegistroDNS(struct DNSRecord dns_database[], int *num_records, const char *domain, const char *ip_address);
+const char *buscarIPPorDominio(struct DNSRecord dns_database[], int num_records, const char *nombreDominio);
+
+
+void agregarRegistroDNS(struct DNSRecord dns_database[], int *num_records, const char *domain, const char *ip_address) {
+  if (*num_records >= 10) {
+    printf("Error: La base de datos de registros DNS está llena\n");
+    return;
+  }
+
+   for (int i = *num_records; i > 0; i--) {
+        strcpy(dns_database[i].domain, dns_database[i - 1].domain);
+        strcpy(dns_database[i].ip_address, dns_database[i - 1].ip_address);
+    }
+
+
+  strcpy(dns_database[0].domain, domain);
+  strcpy(dns_database[0].ip_address, ip_address);
+  (*num_records)++;
+}
+
+const char *buscarIPPorDominio(struct DNSRecord dns_database[], int num_records, const char *nombreDominio) {
+  const char *msg = NULL;
+
+  // Ciclo para buscar dentro de todo el arreglo un registro DNS
+  for (int i = 0; i < num_records; i++) {
+    // Comparamos si el nombre del dominio a buscar es igual al nombre del
+    // dominio de la posición en la que nos encontramos en el arreglo para que
+    // si es igual nos devuelva la IP.
+    if (strcmp(nombreDominio, dns_database[i].domain) == 0) {
+      msg = dns_database[i].ip_address;
+      break;
+    }
+  }
+
+  return msg;
+}
+
+
+
 int main(){
+   struct DNSRecord dns_database[10];
+  int num_records = 0;
+
     //We create the local server
-      printf("Configuring local address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -28,12 +75,11 @@ int main(){
 
      //We create the listener socket
     // We create the socket
-      printf("Creating socket...\n");
     SOCKET socket_listen;
     socket_listen = socket(bind_address->ai_family,
             bind_address->ai_socktype, bind_address->ai_protocol);
     if (!ISVALIDSOCKET(socket_listen)) {
-        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        printf("El socket no se creó\n");
         return 1;
     }
 
@@ -65,8 +111,19 @@ int main(){
     struct addrinfo *ip_address;
     char word[bytes_received+1];
     for (int i=0;i<bytes_received-1;++i) word[i]=read[i];
+
+  num_records++;
     word[bytes_received-1]='\0';
     printf("%s",word);
+    const char* value=buscarIPPorDominio(dns_database, num_records, word);
+
+  if (value){
+    sendto(socket_listen, value, strlen(value), 0, (struct sockaddr*)&client_address,
+    client_len);
+    printf("Sent %s autoritativo\n",value);
+    
+  }else{
+
     int status = getaddrinfo(word, NULL, &hints2, &ip_address);
 if (status != 0) {
     fprintf(stderr, "getaddrinfo() failed: %s\n", gai_strerror(status));
@@ -84,8 +141,12 @@ if (status != 0) {
     int bytes_sent=sendto(socket_listen, address_buffer, sizeof(address_buffer), 0, (struct sockaddr*)&client_address,
     client_len);
     printf("Sent: %s, (%ld)\n",address_buffer,  sizeof(address_buffer));
+    char address2[bytes_sent];
+    for (int j=0;j<bytes_sent-1;++j) address2[j]=address_buffer[j];
+    agregarRegistroDNS(dns_database, &num_records, word,address2);
     freeaddrinfo(ip_address);
-
+    }
+  
   }
     printf("Closing listening socket...\n");
     CLOSESOCKET(socket_listen);
