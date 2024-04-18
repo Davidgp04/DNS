67599863f70937a241df4b5d2082a0d2f92c4a4c
Davidgp04 <104110970+Davidgp04@users.noreply.github.com>
2024-04-18 10:04:09 -0500

    Update udp_client.c

--- udp_client.c
+++ udp_client.c
@@ -19,7 +19,7 @@ int main() {
     set1.ai_socktype=SOCK_DGRAM;
 
     struct addrinfo *listener;
-    if (getaddrinfo("127.0.0.1", "8080", &set1, &listener)){
+    if (getaddrinfo("172.31.83.108", "57", &set1, &listener)){
         printf("Todo salió bien...\n");
     }    
 
